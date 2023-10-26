90f4e314b1a34e48cd3e39fbe7408f11189180d9
minggo <nudtzxm@gmail.com>
2023-10-26 10:37:56 +0800

    improve 3d constrain demo effect

--- assets/cases/scripts/ApplyForce.ts
+++ assets/cases/scripts/ApplyForce.ts
@@ -5,19 +5,44 @@ const { ccclass, property } = _decorator;
 export class ApplyForce extends Component {
     sphere2Node: Node = null;
     toggleNode: Node = null;
+    vecCenter: Vec3;
+
     start () {
         this.toggleNode = this.node.getChildByName('Toggle');
         this.toggleNode.on('toggle', this.callback, this);
 
-        this.applyForce();
+        this.sphere2Node = this.node.parent.getChildByName('Sphere2');
+        this.vecCenter = this.sphere2Node.getWorldPosition();
 
-        // this.schedule(this.applyForce, 10, macro.REPEAT_FOREVER);
+        // this.applyFirstForce();
+
+        this.schedule(this.applyFirstForce, 5, 0);
+
+        this.schedule(this.applyForceRepeat, 10, macro.REPEAT_FOREVER);
     }
 
-    applyForce () {
-        this.sphere2Node = this.node.parent.getChildByName('Sphere2');
+    applyFirstForce () {
         let rigidBody = this.sphere2Node.getComponent(RigidBody);
         rigidBody.applyLocalForce(new Vec3(500, 0, 500), new Vec3(-1, 0, 0));
+    }
+
+    applyForceRepeat () {
+        let rigidBody = this.sphere2Node.getComponent(RigidBody);
+        const currPos = this.sphere2Node.getWorldPosition();
+        if (currPos.x < this.vecCenter.x) {
+            if (currPos.z < this.vecCenter.z) {
+                rigidBody.applyLocalForce(new Vec3(-500, 0, 500), new Vec3(0, 0, 0));
+            } else {
+                rigidBody.applyLocalForce(new Vec3(500, 0, 500), new Vec3(0, 0, 0));
+            } 
+        }
+        else {
+            if (currPos.z < this.vecCenter.z) {
+                rigidBody.applyLocalForce(new Vec3(-500, 0, -500), new Vec3(0, 0, 0));
+            } else {
+                rigidBody.applyLocalForce(new Vec3(500, 0, -500), new Vec3(0, 0, 0));
+            } 
+        }
     }
  
     callback (toggle: Toggle) {
