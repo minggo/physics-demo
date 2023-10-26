c620791bda03ce0d6509ad140f677c24a751c041
minggo <nudtzxm@gmail.com>
2023-10-26 09:14:20 +0800

    improve codes

--- assets/cases/scripts/physics.ts
+++ assets/cases/scripts/physics.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, tween, Sprite, Color, instantiate, Collider2D, Contact2DType, Toggle, ToggleComponent, PhysicsSystem2D } from 'cc';
+import { _decorator, Component,Toggle, PhysicsSystem2D } from 'cc';
 const { ccclass, property, type } = _decorator;
 
 @ccclass('Physics')
@@ -9,7 +9,7 @@ export class Physics extends Component {
        this.toggle.node.on('toggle', this.callback, this);
     }
 
-    callback(toggle: ToggleComponent){
+    callback(toggle: Toggle){
         PhysicsSystem2D.instance.enable = toggle.isChecked;
     }
 
